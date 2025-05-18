265d2b4b795d8cd901b2fe23426322a658e40a53
Majid Nami <79191475+MajidNami@users.noreply.github.com>
2025-05-18 15:40:01 +0330

    Update secret.py

--- database/secret.py
+++ database/secret.py
@@ -1,6 +1,6 @@
 DB_NAME = 'defaultdb'
 DB_USER = 'avnadmin'
-DB_PASSWORD = 'AVNS_QXs1v9qBTveDtLIXZfW'
-DB_HOST = 'mysql-374f4726-majidnamiiiii-e945.a.aivencloud.com'
-DB_PORT = '11741'
-DB_URL = "mysql://avnadmin:AVNS_QXs1v9qBTveDtLIXZfW@mysql-374f4726-majidnamiiiii-e945.a.aivencloud.com:11741/defaultdb?ssl-mode=REQUIRED"
+DB_PASSWORD = 'AVNS_rQ3l1oGYvOGlp1xd8Kf'
+DB_HOST = 'mysql-e00407c-se-fd9e.f.aivencloud.com'
+DB_PORT = '16171'
+DB_URL = "mysql://avnadmin:AVNS_rQ3l1oGYvOGlp1xd8Kf@mysql-e00407c-se-fd9e.f.aivencloud.com:16171/defaultdb?ssl-mode=REQUIRED"
