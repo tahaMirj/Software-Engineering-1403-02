9202804f8fe6ed5ad254ac8690ffbbb871d478db
anitalotfi <anittalotfi@gmail.com>
2025-07-21 11:25:39 +0330

    The signup, login and logout pages for group6 has been connected.

--- group6/urls.py
+++ group6/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+
 app_name = 'group6'
+
 urlpatterns = [
-  path('', views.home, name='group6')
+  path('', views.home, name='group6'),
+  path('signup/', views.group6_signup, name='signup'), 
+  path('login/', views.group6_login, name='login'),   
+  path('logout/', views.group6_logout, name='logout'), 
 
 ] 
--- group6/views.py
+++ group6/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse # Import reverse to get URL by name
 
-# Create your views here.
 def home(request):
-    return render (request , 'group6.html' , {'group_number': '6'})
+    return render(request, 'group6.html', {'group_number': '6'})
 
+# redirect to the registration signup page
+def group6_signup(request):
+    return redirect(reverse('signup'))
+
+# redirect to the registration login page
+def group6_login(request):
+    return redirect(reverse('login'))
+
+# redirect to the registration logout page
+def group6_logout(request):
+    return redirect(reverse('logout'))
