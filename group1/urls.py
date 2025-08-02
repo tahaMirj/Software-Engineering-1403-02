9ae63ca774c3ee410663665a4a0fa909d4606e2e
Mahdi Haeri <mahdihaerim@gmail.com>
2025-08-02 03:30:25 +0330

    CleanUp progress_dashboard

--- group1/urls.py
+++ group1/urls.py
@@ -22,6 +22,4 @@ urlpatterns = [
   path('reset-quiz/<int:quiz_id>/', views.reset_quiz, name='reset_quiz'),
   path('quiz/<int:quiz_id>/review/', views.review_mistakes, name='review_mistakes'),
   path('quiz/<int:quiz_id>/retry/', views.retry_mistakes, name='retry_mistakes'),
-  path('progress/', views.progress_dashboard, name='progress_dashboard'),
-
 ]
